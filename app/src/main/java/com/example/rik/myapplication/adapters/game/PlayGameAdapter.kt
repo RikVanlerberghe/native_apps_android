abfb86cf48ff963e018cecb2ace942d757b9857c
Rik <rik.vanlerberghe.v8896@student.hogent.be>
2018-12-24 15:43:18 +0100

    scorebord werkt

--- app/src/main/java/com/example/rik/myapplication/adapters/game/PlayGameAdapter.kt
+++ app/src/main/java/com/example/rik/myapplication/adapters/game/PlayGameAdapter.kt
@@ -6,21 +6,23 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.example.rik.myapplication.R
+import com.example.rik.myapplication.domain.models.Player
+import com.example.rik.myapplication.fragments.game.PlayGameFragment
 import com.example.rik.myapplication.viewHolders.game.PlayGameViewHolder
-import com.example.rik.myapplication.viewHolders.game.PlayerViewHolder
 
-class PlayGameAdapter(internal var context: Context, internal var fragment: Fragment,
-                      internal var playerList: MutableList<String>)
+class PlayGameAdapter(internal var context: Context,
+                      internal var fragment: PlayGameFragment,
+                      internal var playerList: MutableList<Player>)
     : RecyclerView.Adapter<PlayGameViewHolder>()
 {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PlayGameViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.game_play_game, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.game_scorebord_item, parent, false)
         return PlayGameViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holderCreate: PlayGameViewHolder, position: Int) {
         var member = playerList[position]
-        holderCreate.name.text = member
+
         holderCreate.fifteen.text = "15"
         holderCreate.sixteen.text = "15"
         holderCreate.seventeen.text = "15"
@@ -29,6 +31,7 @@ class PlayGameAdapter(internal var context: Context, internal var fragment: Frag
         holderCreate.twenty.text = "15"
         holderCreate.bull.text = "15"
         holderCreate.score.text = "15"
+        holderCreate.name.text = member.name
 
     }
 
--- app/src/main/java/com/example/rik/myapplication/fragments/game/PlayGameFragment.kt
+++ app/src/main/java/com/example/rik/myapplication/fragments/game/PlayGameFragment.kt
@@ -3,7 +3,6 @@ package com.example.rik.myapplication.fragments.game
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.helper.ItemTouchHelper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +10,12 @@ import com.example.rik.myapplication.R
 import com.example.rik.myapplication.activities.GameActivity
 import com.example.rik.myapplication.adapters.game.PlayGameAdapter
 import com.example.rik.myapplication.domain.models.Player
-import kotlinx.android.synthetic.main.game_create_game.*
 import kotlinx.android.synthetic.main.game_play_game.*
 
 class PlayGameFragment:Fragment() {
 
-    private lateinit var adapter: PlayGameAdapter
     private lateinit var adapterList: MutableList<String>
+    private lateinit var adapter: PlayGameAdapter
     private lateinit var group: ArrayList<Player>
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -25,7 +23,6 @@ class PlayGameFragment:Fragment() {
         val view = inflater.inflate(R.layout.game_play_game, container, false)
 
         group = getMainActivity().getGroup()
-        makeRecyclerList()
 
         return view
     }
@@ -33,17 +30,19 @@ class PlayGameFragment:Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        makeRecyclerList()
     }
 
     private fun makeRecyclerList() {
-        adapterList = ArrayList()
-        group.forEach { player ->
-            adapterList.add(player.name)
-        }
-        adapter = PlayGameAdapter(context!!, this, adapterList)
+        adapter = PlayGameAdapter(context!!, this, group)
+        this.scorebord.adapter = adapter
+        this.scorebord.layoutManager = LinearLayoutManager(context)
+    }
+
+    private fun editAdapterList() {
         scorebord.adapter = adapter
-        //TODO werkt nog niet
+        adapter.notifyDataSetChanged()
+        scorebord.setHasFixedSize(true)
         scorebord.layoutManager = LinearLayoutManager(context)
     }
 
