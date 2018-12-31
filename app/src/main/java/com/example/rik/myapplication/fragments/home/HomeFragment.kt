da522d32d635072c023bf5fbfeeced01caa62184
Rik <rik.vanlerberghe.v8896@student.hogent.be>
2018-12-31 16:08:56 +0100

    nog net home aangepast

--- app/src/main/java/com/example/rik/myapplication/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/example/rik/myapplication/fragments/home/HomeFragment.kt
@@ -22,10 +22,12 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //check if user is logged in
-        if(getMainActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE) == null || getMainActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE).getString("username", "niet ingelogd").equals("")){
+        if(getMainActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE) == null || getMainActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE).getString("username", "").equals("")){
             try {
                 Snackbar.make(view, "You are not logged in", Snackbar.LENGTH_LONG).show()
-            }catch (e: Exception){}        }
+            }catch (e: Exception){}        }else{
+            title_home.text = getMainActivity().getSharedPreferences("myPref", Context.MODE_PRIVATE).getString("username", "")
+        }
         //goTo buy card
         buy_card.setOnClickListener {
             getMainActivity().goTo("buyCard")
